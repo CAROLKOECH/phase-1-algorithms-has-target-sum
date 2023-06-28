67e39de89d518ed38b76db0ce2969a2838ce44b7
CAROLKOECH <koechc44@gmail.com>
2023-06-28 12:08:12 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,34 +1,28 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  // Use a nested loop to iterate over all possible pairs of numbers
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      // Check if the sum of the current pair equals the target
+      if (array[i] + array[j] === target) {
+        return true;
+      }
+    }
+  }
+  
+  // If no pair sums up to the target, return false
+  return false;
 }
 
-/* 
-  Write the Big O time complexity of your function here
-*/
+// Test Cases
+console.log("Expecting: true");
+console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
-/* 
-  Add your pseudocode here
-*/
+console.log("");
 
-/*
-  Add written explanation of your solution here
-*/
+console.log("Expecting: true");
+console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
 
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+console.log("");
 
-  console.log("");
-
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
-}
-
-module.exports = hasTargetSum;
+console.log("Expecting: false");
+console.log("=>", hasTargetSum([1, 2, 5], 4));
